0b891f32f6c4d2c44db0abdde72384a66c7e1d55
zakariakebour-arch <zakariakebour@gmail.com>
2026-03-31 10:57:47 +0200

    nueva version con conexion a la base de datos como ejemplo

---
+++ back/app/core/database.py
@@ -0,0 +1,32 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# URL de conexion a MySQL
+DATABASE_URL = "mysql+pymysql://user:password@localhost:3306/marketplace"
+
+#Conexion base
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=10,
+    max_overflow=20
+)
+
+#Cada peticion usa una
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+#Base para modelos ORM
+Base = declarative_base()
+
+
+#Dependencia para FastAPI
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ back/app/moduls/users/modules.py
@@ -0,0 +1,35 @@
+from sqlalchemy import Column, String, Boolean, Enum
+from core.database import Base
+import enum
+import uuid
+
+
+class UserRole(enum.Enum):
+    admin = "admin"
+    owner = "owner"
+    customer = "customer"
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    # ID seguro (no incremental)
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+
+    # username visible
+    username = Column(String(50), unique=True, nullable=False, index=True)
+
+    # email para login
+    email = Column(String(255), unique=True, nullable=False, index=True)
+
+    # contraseña hasheada
+    hashed_password = Column(String(255), nullable=False)
+
+    # rol del usuario
+    role = Column(Enum(UserRole), nullable=False, default=UserRole.customer)
+
+    # estado
+    is_active = Column(Boolean, default=True)
+
+
+print("Hola mundo")
